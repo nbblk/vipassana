d829ee4f443c576f457bc7cf8affb3c441d9fccf
nbblk <nbblks@gmail.com>
2024-06-20 23:18:11 -0700

    feat: add footer and slider

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center p-24 text-white">
+      <p>
+        Made with{" "}
+        <span role="img" aria-label="love">
+          ❤️
+        </span>{" "}
+        by <a href="https://linkedin.com/in/blake-sim" >Blake Sim</a>
+      </p>
+    </footer>
+  );
+}
